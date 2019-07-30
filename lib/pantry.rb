082ad4f17902fbf7abb15594602089b1c71a0d9f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-30 11:37:39 -0600

    Complete enough_ingredients_for_method in pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -14,13 +14,7 @@ class Pantry
   end
 
   def enough_ingredients_for?(recipe)
-    # @stock
-    # recipe_keys = recipe.ingredients_required
-    # (recipe.ingredients_required.keys & @stock.keys).size == @stock.keys.size
-    # recipe.ingredients_required.keys
-    # (recipe.ingredients_required.keys & @stock.keys).size == recipe.ingredients_required.keys.size
     positive_differences = []
-    positive_differences.count
     ingredient_differences = recipe.ingredients_required.merge!(@stock){|k, v1, v2| v1 - v2 }.values
     ingredient_differences.each do |difference|  #.all?(&:negative?)
       positive_differences << difference if difference > 0
@@ -29,10 +23,6 @@ class Pantry
       return true
     else
       return false
-    # if ingredient_differences.all?(&:positive?) && recipe.ingredients_required.keys.all?{|i| @stock.keys.include?(i)}
-    #   return true
-    # else ingredient_differences.any?(&:negative?) || recipe.ingredients_required.keys.all?{|i| @stock.keys.include?(i)}
-    #   return false
     end
   end
 end
