72c802766cb9e0a1b8bc7a11850a87cf5cf774dd
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-30 09:51:20 -0600

    Add pantry class and test files

---
+++ test/pantry_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+    @pantry = Pantry.new
+  end
+
+  def test_that_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_that_it_has_attributes
+    assert_equal ({}), @pantry.stock
+  end
+end
