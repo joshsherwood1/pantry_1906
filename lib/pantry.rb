388b04026fa6965f4fe7bb3d2a7028ad7b617fba
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-07-30 10:56:14 -0600

    Add more conditions to enough ingredients for method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -14,11 +14,12 @@ class Pantry
   end
 
   def enough_ingredients_for?(recipe)
+    # recipe.ingredients_required.keys
     # (recipe.ingredients_required.keys & @stock.keys).size == recipe.ingredients_required.keys.size
     ingredient_differences = @stock.merge!(recipe.ingredients_required){|k, v1, v2| v1 - v2 }.values
-    if ((recipe.ingredients_required.keys & @stock.keys).size == recipe.ingredients_required.keys.size) && ingredient_differences.all?(&:positive?)
+    if ingredient_differences.all?(&:positive?) && (recipe.ingredients_required.keys & @stock.keys).size == recipe.ingredients_required.keys.size
       return true
-    else ((recipe.ingredients_required.keys & @stock.keys).size == recipe.ingredients_required.keys.size) == false || ingredient_differences.any?(&:negative?)
+    else ingredient_differences.any?(&:negative?) || (recipe.ingredients_required.keys & @stock.keys).size != recipe.ingredients_required.keys.size
       return false
     end
   end
